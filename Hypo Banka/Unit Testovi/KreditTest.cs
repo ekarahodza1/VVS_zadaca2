418c5ad4945c4c68c80b8e5fa394e266f6341b0c
Dzenana <dhuseinspa1@etf.unsa.ba>
2020-11-25 00:51:18 +0100

    test za metodu ProvjeriStanjeOtplate

---
+++ Hypo Banka/Unit Testovi/KreditTest.cs
@@ -0,0 +1,19 @@
+﻿using Hypo_Banka;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Unit_Testovi
+{
+    [TestClass]
+    public class KreditTest
+    {
+        [TestMethod] 
+        public void Test()
+        {
+            Klijent klijent = new Klijent();
+            Kredit kredit = new Kredit(klijent, 10000, 150, 0.05, DateTime.Parse("12.12.2020."));
+            Assert.AreEqual(kredit.ProvjeriStanjeOtplate(), "Kredit koji se treba vratiti najkasnije na dan 12.12.2020. godine " +
+                "ima preostali iznos od 10000KM. Iznos rate je 150KM, po stopi od 5%.");
+        }
+    }
+}
